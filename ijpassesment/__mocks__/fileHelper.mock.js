6014f35485ea0af7c2504d288d884262cb6348ed
Prasad Kanaparthi <prasadmcts@gmail.com>
2021-04-08 23:59:57 +0530

    Delete ijpassesment directory

--- ijpassesment/__mocks__/fileHelper.mock.js
+++
@@ -1,3 +0,0 @@
-let mockData = require('./testData.mock');
-
-export default jest.fn(() => mockData.creditlimitsDataSet);
--- ijpassesment/helper/constants.js
+++
@@ -1,8 +0,0 @@
-var Constants = {
-    NolimitBreaches: "No limit breaches",
-    LimitBreaches: "Limit breach at {0} (limit = {1}, direct utilisation = {2}, combined utilisation = {3}).",
-    EntitySeparator: "/",
-    Entities: "Entities: "
-}
-
-module.exports.Constants = Constants;
