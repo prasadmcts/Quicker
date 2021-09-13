f64564f7ce18916d52d56500bc48ae11af3e1487
Prasad Kanaparthi <prasadmcts@gmail.com>
2021-09-13 16:00:31 +0530

    Update Crypto.js

--- Crypto.js
+++ Crypto.js
@@ -1,1 +1,149 @@
+http://nodejs.org/docs/latest/api/crypto.html
 
+var crypto = require('crypto');
+var assert = require('assert');
+
+var algorithm = 'aes256'; // or any other algorithm supported by OpenSSL
+var key = 'password';
+var text = 'I love kittens';
+
+var cipher = crypto.createCipher(algorithm, key);  
+var encrypted = cipher.update(text, 'utf8', 'hex') + cipher.final('hex');
+var decipher = crypto.createDecipher(algorithm, key);
+var decrypted = decipher.update(encrypted, 'hex', 'utf8') + decipher.final('utf8');
+
+assert.equal(decrypted, text);
+
+========
+
+const crypto = require('crypto');
+
+var exports = module.exports = {};
+
+const ALGORITHM = {
+    
+    /**
+     * GCM is an authenticated encryption mode that
+     * not only provides confidentiality but also 
+     * provides integrity in a secured way
+     * */  
+    BLOCK_CIPHER: 'aes-256-gcm',
+
+    /**
+     * 128 bit auth tag is recommended for GCM
+     */
+    AUTH_TAG_BYTE_LEN: 16,
+
+    /**
+     * NIST recommends 96 bits or 12 bytes IV for GCM
+     * to promote interoperability, efficiency, and
+     * simplicity of design
+     */
+    IV_BYTE_LEN: 12,
+
+    /**
+     * Note: 256 (in algorithm name) is key size. 
+     * Block size for AES is always 128
+     */
+    KEY_BYTE_LEN: 32,
+
+    /**
+     * To prevent rainbow table attacks
+     * */
+    SALT_BYTE_LEN: 16
+}
+
+const getIV = () => crypto.randomBytes(ALGORITHM.IV_BYTE_LEN);
+exports.getRandomKey = getRandomKey = () => crypto.randomBytes(ALGORITHM.KEY_BYTE_LEN);
+
+/**
+ * To prevent rainbow table attacks
+ * */
+exports.getSalt = getSalt = () => crypto.randomBytes(ALGORITHM.SALT_BYTE_LEN);
+
+/**
+ * 
+ * @param {Buffer} password - The password to be used for generating key
+ * 
+ * To be used when key needs to be generated based on password.
+ * The caller of this function has the responsibility to clear 
+ * the Buffer after the key generation to prevent the password 
+ * from lingering in the memory
+ */
+exports.getKeyFromPassword = getKeyFromPassword = (password, salt) => {
+    return crypto.scryptSync(password, salt, ALGORITHM.KEY_BYTE_LEN);
+}
+
+/**
+ * 
+ * @param {Buffer} messagetext - The clear text message to be encrypted
+ * @param {Buffer} key - The key to be used for encryption
+ * 
+ * The caller of this function has the responsibility to clear 
+ * the Buffer after the encryption to prevent the message text 
+ * and the key from lingering in the memory
+ */
+exports.encrypt = encrypt = (messagetext, key) => {
+    const iv = getIV();
+    const cipher = crypto.createCipheriv(
+        ALGORITHM.BLOCK_CIPHER, key, iv,
+        { 'authTagLength': ALGORITHM.AUTH_TAG_BYTE_LEN });
+    let encryptedMessage = cipher.update(messagetext);
+    encryptedMessage = Buffer.concat([encryptedMessage, cipher.final()]);
+    return Buffer.concat([iv, encryptedMessage, cipher.getAuthTag()]);
+}
+
+/**
+ * 
+ * @param {Buffer} ciphertext - Cipher text
+ * @param {Buffer} key - The key to be used for decryption
+ * 
+ * The caller of this function has the responsibility to clear 
+ * the Buffer after the decryption to prevent the message text 
+ * and the key from lingering in the memory
+ */
+exports.decrypt = decrypt = (ciphertext, key) => {
+    const authTag = ciphertext.slice(-16);
+    const iv = ciphertext.slice(0, 12);
+    const encryptedMessage = ciphertext.slice(12, -16);
+    const decipher = crypto.createDecipheriv(
+        ALGORITHM.BLOCK_CIPHER, key, iv,
+        { 'authTagLength': ALGORITHM.AUTH_TAG_BYTE_LEN });
+    decipher.setAuthTag(authTag);
+    let messagetext = decipher.update(encryptedMessage);
+    messagetext = Buffer.concat([messagetext, decipher.final()]);
+    return messagetext;
+}
+=============
+
+const assert = require('assert');
+const cryptoUtils = require('../lib/crypto_utils');
+describe('CryptoUtils', function() {
+  describe('decrypt()', function() {
+    it('should return the same mesage text after decryption of text encrypted with a '
+     + 'randomly generated key', function() {
+      let plaintext = 'my message text';
+      let key = cryptoUtils.getRandomKey();
+      let ciphertext = cryptoUtils.encrypt(plaintext, key);
+
+      let decryptOutput = cryptoUtils.decrypt(ciphertext, key);
+
+      assert.equal(decryptOutput.toString('utf8'), plaintext);
+    });
+
+    it('should return the same mesage text after decryption of text excrypted with a '
+     + 'key generated from a password', function() {
+      let plaintext = 'my message text';
+      /**
+       * Ideally the password would be read from a file and will be in a Buffer
+       */
+      let key = cryptoUtils.getKeyFromPassword(
+              Buffer.from('mysecretpassword'), cryptoUtils.getSalt());
+      let ciphertext = cryptoUtils.encrypt(plaintext, key);
+
+      let decryptOutput = cryptoUtils.decrypt(ciphertext, key);
+
+      assert.equal(decryptOutput.toString('utf8'), plaintext);
+    });
+  });
+});
